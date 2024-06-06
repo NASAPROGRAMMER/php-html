dc187e4983dba142150003208d07bed98ad7fdde
NASAPROGRAMMER <rangkay57@gmail.com>
2024-06-06 18:29:57 +0700

    Create tes.php

---
+++ tes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Judul</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+</head>
+<body>
+<?php
+  $kata = "Nama kamu siapa";
+  echo $kata;
+  ?>
+</body>
+</html>
