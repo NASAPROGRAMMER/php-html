f54274d5f641211693cb84046cabdfa6ef8e242e
Rangggggggggggggggggggggggggggggga <rangkay57@gmail.com>
2025-09-07 16:09:55 +0700

    Update Legal33.lua

--- Legal33.lua
+++ Legal33.lua
@@ -109,7 +109,7 @@ makeMenu("🤯 Teleport Maju 10.000M",10,function()
     local hrp = character:WaitForChild("HumanoidRootPart")
     
     -- teleport maju 10.000 meter sesuai arah pandangan
-    hrp.CFrame = hrp.CFrame + hrp.CFrame.LookVector * 1000
+    hrp.CFrame = hrp.CFrame + hrp.CFrame.LookVector * 150
     
     DetailText.Text = "Kamu teleport 1.000 meter! 🌴"
 end)
